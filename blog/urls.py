45b3d2e1b1e4e8ed11a3839886acac7d6eee3212
SunSki <everyday.natsuki@gmail.com>
2019-04-18 02:57:31 +0900

    view test text4

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/new/', views.post_new, name='post_new'),
-    path('txtGen/', views.txtGen, name='txtGen'),
+    # path('txtGen/', views.txtGen, name='txtGen'),
 ]
