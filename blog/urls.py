75ae9c3e5de70c86c6c520f915a6effc0fffa999
SunSki <everyday.natsuki@gmail.com>
2019-04-17 16:22:17 +0900

    added view and template for detailed blog post

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # if ~:8000/ -> wiews.post_listにルーティング
+    # if '/' -> wiews.pyのpost_list にルーティング
     # name='post_list' は、ビューを識別するために使われるURL の名前
-    path('', views.post_list, name = 'post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
